38b84c1fe186fe7247cf00f5dff296ddb53b2bf0
Todd Tanner <todd@spawndev.com>
2026-08-07 23:35:18 -0400

    Depenedency update

--- Examples/TolerableTubeSimple/wwwroot/content.js
+++ Examples/TolerableTubeSimple/wwwroot/content.js
@@ -1,21 +1,5 @@
-﻿// Script loader works in window and worker scopes
+﻿// content.js
 (async function () {
-    async function loadScript(src) {
-        src = chrome.runtime.getURL(src);
-        if (typeof globalThis.importScripts === 'function') {
-            globalThis.importScripts(src);
-        } else if (globalThis.document) {
-            const script = document.createElement('script');
-            const loadTask = new Promise((onload, onerror) => Object.assign(script, { onload, onerror, src }));
-            (document.head || docuemnt.documentElement).append(script);
-            await loadTask;
-        }
-    }
-    // Load anything that needs to load before .Net Wasm
-    //
-    // Synchronously fired events need to be captured by Javascript and
-    // held for .Net Wasm to pick up and handle once it loads.
-    // 
-    // Load .Net Wasm app
-    await loadScript('app/main.classic.js');
+    // Load .Net app
+    await import(chrome.runtime.getURL('app/main.module.js'));
 })();
